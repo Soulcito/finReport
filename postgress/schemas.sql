854ce819d95839a2f9ac7ecb22463ae4d381b7b9
Felipe <fepipe82@hotmail.com>
2025-10-26 14:38:31 -0300

    Avance de creacion de schemas y tablas para interfaces

---
+++ postgress/schemas.sql
@@ -0,0 +1,80 @@
+-- Creacion de schema interface, usado para las interfaces
+DO
+$$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.schemata
+        WHERE schema_name = 'interface'
+    ) THEN
+        EXECUTE 'DROP SCHEMA interface CASCADE';
+    END IF;
+
+    EXECUTE 'CREATE SCHEMA interface';
+END;
+$$;
+
+-- Creacion de schema temporal, usado para la carga de interfaces
+DO
+$$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.schemata
+        WHERE schema_name = 'tmp'
+    ) THEN
+        EXECUTE 'DROP SCHEMA tmp CASCADE';
+    END IF;
+
+    EXECUTE 'CREATE SCHEMA tmp';
+END;
+$$;
+
+-- Creacion de schema log, usado para los log de la carga de las interfaces
+DO
+$$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.schemata
+        WHERE schema_name = 'log'
+    ) THEN
+        EXECUTE 'DROP SCHEMA log CASCADE';
+    END IF;
+
+    EXECUTE 'CREATE SCHEMA log';
+END;
+$$;
+
+-- Creacion de schema historico, usado para almacenar la historia de los datos
+DO
+$$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.schemata
+        WHERE schema_name = 'historico'
+    ) THEN
+        EXECUTE 'DROP SCHEMA historico CASCADE';
+    END IF;
+
+    EXECUTE 'CREATE SCHEMA historico';
+END;
+$$;
+
+-- Creacion de schema respaldo, usado para almacenar el respaldo de los datos
+DO
+$$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.schemata
+        WHERE schema_name = 'respaldo'
+    ) THEN
+        EXECUTE 'DROP SCHEMA respaldo CASCADE';
+    END IF;
+
+    EXECUTE 'CREATE SCHEMA respaldo';
+END;
+$$;
+
---
+++ postgress/tables_interface.sql
@@ -0,0 +1,145 @@
+-- TABLAS PARA LAS INTERFACES
+
+DO
+$$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.tables
+        WHERE table_schema = 'tmp'
+        AND table_name = 'cartera_operaciones'
+    ) THEN
+        EXECUTE 'DROP TABLE tmp.cartera_operaciones';
+    END IF;
+END;
+$$;
+
+-- Tabla tmp.cartera_operaciones
+
+CREATE TABLE tmp.cartera_operaciones (
+	 fila                       integer not null 
+	,fecha_proceso              varchar(250) null
+	,rut                        varchar(250) null
+	,cod_persona                varchar(250) null
+	,cod_operacion              varchar(250) null
+	,cod_titulo_3               varchar(250) null
+	,cod_deudor                 varchar(250) null
+	,cod_tipo_obligacion        varchar(250) null
+	,fecha_otorgamiento         varchar(250) null
+	,carga_financiera           varchar(250) null
+	,fecha_extincion            varchar(250) null
+	,gar_real_inmobiliaria      varchar(250) null
+	,gar_real_mobiliaria        varchar(250) null
+	,gar_financiera             varchar(250) null
+	,gar_personal               varchar(250) null
+	,monto_original             varchar(250) null
+	,capital                    varchar(250) null
+	,interes                    varchar(250) null
+	,otros                      varchar(250) null  
+	,cod_moneda                 varchar(250) null 
+	,fecha_aceleracion          varchar(250) null
+	,deuda_renegociada          varchar(250) null
+	,operación_desfasada        varchar(250) null
+	,fecha_a_rectificar         varchar(250) null
+	,fecha_rectificacion        varchar(250) null
+	,causal_rectificacion       varchar(250) null
+	,numero_solicitud           varchar(250) null
+	,valor_contable             varchar(250) null
+);
+
+--log para cartera_operaciones
+
+DO
+$$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.tables
+        WHERE table_schema = 'log'
+        AND table_name = 'cartera_operaciones'
+    ) THEN
+        EXECUTE 'DROP TABLE log.cartera_operaciones';
+    END IF;
+END;
+$$;
+
+-- Tabla log.cartera_operaciones
+
+CREATE TABLE log.cartera_operaciones (
+	 fila                       integer not null 
+	,fecha_proceso              varchar(250) null
+	,rut                        varchar(250) null
+	,cod_persona                varchar(250) null
+	,cod_operacion              varchar(250) null
+	,cod_titulo_3               varchar(250) null
+	,cod_deudor                 varchar(250) null
+	,cod_tipo_obligacion        varchar(250) null
+	,fecha_otorgamiento         varchar(250) null
+	,carga_financiera           varchar(250) null
+	,fecha_extincion            varchar(250) null
+	,gar_real_inmobiliaria      varchar(250) null
+	,gar_real_mobiliaria        varchar(250) null
+	,gar_financiera             varchar(250) null
+	,gar_personal               varchar(250) null
+	,monto_original             varchar(250) null
+	,capital                    varchar(250) null
+	,interes                    varchar(250) null
+	,otros                      varchar(250) null  
+	,cod_moneda                 varchar(250) null 
+	,fecha_aceleracion          varchar(250) null
+	,deuda_renegociada          varchar(250) null
+	,operación_desfasada        varchar(250) null
+	,fecha_a_rectificar         varchar(250) null
+	,fecha_rectificacion        varchar(250) null
+	,causal_rectificacion       varchar(250) null
+	,numero_solicitud           varchar(250) null
+	,valor_contable             varchar(250) null
+	,problema                   varchar(2000) null
+);
+
+
+DO
+$$
+BEGIN
+    IF EXISTS (
+        SELECT 1
+        FROM information_schema.tables
+        WHERE table_schema = 'interface'
+        AND table_name = 'cartera_operaciones'
+    ) THEN
+        EXECUTE 'DROP TABLE interface.cartera_operaciones';
+    END IF;
+END;
+$$;
+
+-- Tabla interface.cartera_operaciones
+
+CREATE TABLE interface.cartera_operaciones (
+	 fecha_proceso              date not null
+	,rut                        varchar(20) not null
+	,cod_persona                varchar(50) not null
+	,cod_operacion              varchar(50) not null
+	,cod_titulo_3               varchar(50) not null
+	,cod_deudor                 varchar(50) not null
+	,cod_tipo_obligacion        varchar(50) not null
+	,fecha_otorgamiento         date not null
+	,carga_financiera           numeric(22,4) not null
+	,fecha_extincion            date not null
+	,gar_real_inmobiliaria      numeric(22,4) not null
+	,gar_real_mobiliaria        numeric(22,4) not null
+	,gar_financiera             numeric(22,4) not null
+	,gar_personal               numeric(22,4) not null
+	,monto_original             numeric(22,4) not null
+	,capital                    numeric(22,4) not null
+	,interes                    numeric(22,4) not null
+	,otros                      numeric(22,4) not null  
+	,cod_moneda                 varchar(50) not null 
+	,fecha_aceleracion          date not null
+	,deuda_renegociada          varchar(2) not null
+	,operación_desfasada        varchar(2) not null
+	,fecha_a_rectificar         date not null
+	,fecha_rectificacion        date not null
+	,causal_rectificacion       varchar(2) not null
+	,numero_solicitud           varchar(50) not null
+	,valor_contable             numeric(22,4) not null	
+);
