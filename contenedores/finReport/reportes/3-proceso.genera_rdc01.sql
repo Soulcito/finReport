4e941f0e97e850f9ecd40ec3d17541187a28e4b9
Felipe <fepipe82@hotmail.com>
2025-11-17 22:15:11 -0300

    Avance en logica de mora con aceleracion, monto al dia y monto actual

--- contenedores/finReport/reportes/3-proceso.genera_rdc01.sql
+++ contenedores/finReport/reportes/3-proceso.genera_rdc01.sql
@@ -36,7 +36,8 @@ BEGIN
 			a.gar_financiera::NUMERIC(15)                           					as "valor_gtia_financiera", 
 			a.gar_personal::NUMERIC(15)                             					as "valor_gtia_personal", 
 			(a.monto_original * f.valor)::NUMERIC(15)                 					as "monto_original", 
-			(((a.capital + a.interes) * f.valor) + otros)::NUMERIC(15)  				as "monto_actual", 
+			--(((a.capital + a.interes) * f.valor) + otros)::NUMERIC(15)  				as "monto_actual", 
+			0                                                                           as "monto_actual",       -- Se saca mejor del cuadro de pago para que quede todo cuadrado. 
 			0                                                           				as "monto_al_dia",
 			0																			as "monto_mora_1_tramo",
 			0																			as "monto_mora_2_tramo",
@@ -67,7 +68,7 @@ BEGIN
 		   
 
 
-		-- | Actualiza mora sin aceleracion | --
+		-- | Actualiza mora SIN aceleracion | --
 
 		update reporte.rdc01_detalle as a 
 		    set 
@@ -84,8 +85,8 @@ BEGIN
 			 select
 			    BASE.cod_operacion
 			   ,tramo
-			   ,sum(mora_moneda_origen) as "mora"
-			   ,sum(BASE.otros)              as "otros"
+			   ,(sum(mora_moneda_origen) * c.valor)::numeric(15) as "mora"
+			   ,sum(BASE.otros)::numeric(15)                     as "otros"
 			   from (
 						select 
 						    cod_operacion
@@ -104,7 +105,7 @@ BEGIN
 							       and fecha_cuota > make_date(extract(year from fecha_proceso)::int - 4, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)  then 8
 							  when fecha_cuota <= make_date(extract(year from fecha_proceso)::int - 4, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)     then 9  
 							else 1 end  as "tramo"
-						   ,(capital - capital_pagado + interes_devengado + interes_moroso - interes_pagado) as "mora_moneda_origen"
+						   ,(capital - capital_pagado + interes_termino + interes_moroso - interes_pagado) as "mora_moneda_origen"
 						   ,otros                                 
 						    from interface.cuadro_operaciones  
 							     where fecha_cuota <= fecha_proceso
@@ -114,6 +115,126 @@ BEGIN
 					group by BASE.cod_operacion, tramo
 				) AS GBASE 
 				where a.cod_operacion = GBASE.cod_operacion;
+
+
+		-- | Actualiza mora CON aceleracion | --
+
+		update reporte.rdc01_detalle as a 
+		    set 
+			  a.monto_mora_1_tramo = case when GBASE.tramo = 1 then GBASE.mora + GBASE.otros else 0 end
+		     ,a.monto_mora_2_tramo = case when GBASE.tramo = 2 then GBASE.mora + GBASE.otros else 0 end
+		     ,a.monto_mora_3_tramo = case when GBASE.tramo = 3 then GBASE.mora + GBASE.otros else 0 end
+		     ,a.monto_mora_4_tramo = case when GBASE.tramo = 4 then GBASE.mora + GBASE.otros else 0 end
+		     ,a.monto_mora_5_tramo = case when GBASE.tramo = 5 then GBASE.mora + GBASE.otros else 0 end
+		     ,a.monto_mora_6_tramo = case when GBASE.tramo = 6 then GBASE.mora + GBASE.otros else 0 end
+		     ,a.monto_mora_7_tramo = case when GBASE.tramo = 7 then GBASE.mora + GBASE.otros else 0 end
+		     ,a.monto_mora_8_tramo = case when GBASE.tramo = 8 then GBASE.mora + GBASE.otros else 0 end
+		     ,a.monto_mora_9_tramo = case when GBASE.tramo = 1 then GBASE.mora + GBASE.otros else 0 end		   
+		from (
+			 select
+			    BASE.cod_operacion
+			   ,tramo
+			   ,(sum(mora_moneda_origen) * c.valor)::numeric(15) as "mora"
+			   ,sum(BASE.otros)::numeric(15)                     as "otros"
+			   from (
+						select 
+						    cod_operacion
+						   ,
+	                        case
+							  -- Mantiene la mora antigua con la fecha de proceso
+							  when fecha_cuota < fecha_aceleracion then
+		 						   case
+								      when (fecha_proceso - fecha_cuota) + 1 < 30                                                  then 1
+									  when (fecha_proceso - fecha_cuota) + 1 >= 30  and (fecha_proceso - fecha_cuota) + 1 < 60      then 2
+									  when (fecha_proceso - fecha_cuota) + 1 >= 60  and (fecha_proceso - fecha_cuota) + 1 < 90      then 3
+									  when (fecha_proceso - fecha_cuota) + 1 >= 90  and (fecha_proceso - fecha_cuota) + 1 < 180     then 4
+									  when (fecha_proceso - fecha_cuota) + 1 >= 180 
+									       and fecha_cuota > make_date(extract(year from fecha_proceso)::int - 1, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int) then 5
+									  when fecha_cuota <= make_date(extract(year from fecha_proceso)::int - 1, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)
+									       and fecha_cuota > make_date(extract(year from fecha_proceso)::int - 2, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)  then 6
+									  when fecha_cuota <= make_date(extract(year from fecha_proceso)::int - 2, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)
+									       and fecha_cuota > make_date(extract(year from fecha_proceso)::int - 3, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)  then 7
+									  when fecha_cuota <= make_Date(extract(year from fecha_proceso)::int - 3, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)
+									       and fecha_cuota > make_date(extract(year from fecha_proceso)::int - 4, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)  then 8
+									  when fecha_cuota <= make_date(extract(year from fecha_proceso)::int - 4, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)     then 9  
+									else 1 end
+                              else
+							  -- Nueva mora acelerada para las cuotas futuras
+		 						   case
+								      when (fecha_proceso - fecha_aceleracion) + 1 < 30                                                  then 1
+									  when (fecha_proceso - fecha_aceleracion) + 1 >= 30  and (fecha_proceso - fecha_aceleracion) + 1 < 60      then 2
+									  when (fecha_proceso - fecha_aceleracion) + 1 >= 60  and (fecha_proceso - fecha_aceleracion) + 1 < 90      then 3
+									  when (fecha_proceso - fecha_aceleracion) + 1 >= 90  and (fecha_proceso - fecha_aceleracion) + 1 < 180     then 4
+									  when (fecha_proceso - fecha_aceleracion) + 1 >= 180 
+									       and fecha_aceleracion > make_date(extract(year from fecha_proceso)::int - 1, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int) then 5
+									  when fecha_aceleracion <= make_date(extract(year from fecha_proceso)::int - 1, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)
+									       and fecha_aceleracion > make_date(extract(year from fecha_proceso)::int - 2, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)  then 6
+									  when fecha_aceleracion <= make_date(extract(year from fecha_proceso)::int - 2, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)
+									       and fecha_aceleracion > make_date(extract(year from fecha_proceso)::int - 3, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)  then 7
+									  when fecha_aceleracion <= make_Date(extract(year from fecha_proceso)::int - 3, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)
+									       and fecha_aceleracion > make_date(extract(year from fecha_proceso)::int - 4, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)  then 8
+									  when fecha_aceleracion <= make_date(extract(year from fecha_proceso)::int - 4, extract(month from fecha_proceso)::int, extract(day from fecha_proceso)::int)     then 9  
+									else 1 end
+							 end  as "tramo"							  
+						   ,(capital - capital_pagado + interes_termino + interes_moroso - interes_pagado) as "mora_moneda_origen"
+						   ,otros                                 
+						    from interface.cuadro_operaciones  
+					) as BASE   inner join interface.cartera_operaciones b on BASE.cod_operacion = b.cod_operacion
+					            inner join interface.tipo_cambio c         on b.cod_moneda = c.cod_moneda
+					  where to_char(b.fecha_aceleracion,'YYYYMMDD')::VARCHAR <> '19000101'
+					group by BASE.cod_operacion, tramo
+				) AS GBASE 
+				where a.cod_operacion = GBASE.cod_operacion;				
+
+
+
+		-- | Actualiza monto al dia | --
+
+		update reporte.rdc01_detalle as a 
+		    set 
+			  a.monto_al_dia = GBASE.monto + GBASE.otros 
+			   - a.monto_mora_1_tramo 		   
+			   - a.monto_mora_2_tramo
+			   - a.monto_mora_3_tramo
+			   - a.monto_mora_4_tramo
+			   - a.monto_mora_5_tramo
+			   - a.monto_mora_6_tramo
+			   - a.monto_mora_7_tramo
+			   - a.monto_mora_8_tramo
+			   - a.monto_mora_9_tramo
+		from (
+			 select
+			    BASE.cod_operacion
+			   ,tramo
+			   ,(sum(monto_origen) * c.valor)::numeric(15) as "monto"
+			   ,sum(BASE.otros)::numeric(15)               as "otros"
+			   from (
+						select 
+						    cod_operacion
+						   ,(capital - capital_pagado + interes_termino + interes_moroso - interes_pagado) as "monto_origen"
+						   ,otros                                 
+						    from interface.cuadro_operaciones  
+					) as BASE   inner join interface.cartera_operaciones b on BASE.cod_operacion = b.cod_operacion
+					            inner join interface.tipo_cambio c         on b.cod_moneda = c.cod_moneda
+					  where to_char(b.fecha_aceleracion,'YYYYMMDD')::VARCHAR = '19000101'
+					group by BASE.cod_operacion, tramo
+				) AS GBASE 
+				where a.cod_operacion = GBASE.cod_operacion;								
+
+		-- | Actualiza monto actual | --
+
+		update reporte.rdc01_detalle
+		    set 
+			  monto_actual = monto_al_dia 
+			        + monto_mora_1_tramo 
+					+ monto_mora_2_tramo 
+					+ monto_mora_3_tramo
+					+ monto_mora_4_tramo
+					+ monto_mora_5_tramo
+					+ monto_mora_6_tramo
+					+ monto_mora_7_tramo
+					+ monto_mora_8_tramo
+					+ monto_mora_9_tramo;
 
 		
 	EXCEPTION WHEN OTHERS THEN
