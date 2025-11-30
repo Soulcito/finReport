d42831747ace8a949a06646af5b579320249a0b3
Felipe Hinojosa <fepipe82@hotmail.com>
2025-11-30 12:46:47 -0300

    Issue #32
    Se crea procedimiento para validar la interfaz tipo_cambio

--- contenedores/finReport/init.sql
+++ contenedores/finReport/init.sql
@@ -31,6 +31,7 @@
 \ir /docker-entrypoint-initdb.d/procesos/2-proceso.val_interfaz_a01.sql
 \ir /docker-entrypoint-initdb.d/procesos/3-proceso.val_interfaz_b01.sql
 \ir /docker-entrypoint-initdb.d/procesos/4-proceso.trunca_tablas.sql
+\ir /docker-entrypoint-initdb.d/procesos/5-proceso.val_interfaz_c01.sql
 
 -- Ejecutar scripts de la carpeta reportes
 \echo '=== Ejecutando scripts de REPORTES ==='
