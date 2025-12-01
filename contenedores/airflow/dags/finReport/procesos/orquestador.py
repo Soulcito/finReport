1defc5b155656d865b93940593746c2c342927e0
Felipe Hinojosa <fepipe82@hotmail.com>
2025-11-30 21:42:22 -0300

    Revert "Reapply "Issue #36""
    This reverts commit b593879306406ceeca5ebc6063f166e6c9f109f0.

--- contenedores/airflow/dags/finReport/procesos/orquestador.py
+++ contenedores/airflow/dags/finReport/procesos/orquestador.py
@@ -5,8 +5,7 @@ from finReport.procesos import (
     limpia_tablas,
     import_mantenedores,
     import_interfaz,
-    valida_interfaces,
-    historico_log
+    valida_interfaces
 )
 
 with DAG(
@@ -22,6 +21,5 @@ with DAG(
     g2 = import_mantenedores.build_group(dag)
     g3 = import_interfaz.build_group(dag)
     g4 = valida_interfaces.build_group(dag)
-    g5 = historico_log.build_group(dag)
 
-    g1 >> g2 >> g3 >> g4 >> g5
+    g1 >> g2 >> g3 >> g4
