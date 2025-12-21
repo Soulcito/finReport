4a7396058647e49223404b9456d0f9597fc18b8a
Felipe Hinojosa <fepipe82@hotmail.com>
2025-12-20 22:14:02 -0300

    Issue #62
    Se mejora exporta_reportes_excel.py para que incluya otro excel con el detalle.

--- contenedores/airflow/dags/finReport/procesos/exporta_reportes_excel.py
+++ contenedores/airflow/dags/finReport/procesos/exporta_reportes_excel.py
@@ -6,29 +6,43 @@ from finReport.lib.db_config import PG_CONN
 
 
 # ==============================
-# CONFIGURACIÓN
+# CONFIGURACIÓN REPORTES
 # ==============================
 
-GROUP_NAME = "exporta_reportes_excel"
+GROUP_NAME_REPORTE = "exporta_reportes_excel"
 EXCEL_DIR = "/opt/airflow/finReport/reports"
-NOMBRE_EXCEL = "Reportes"
+NOMBRE_EXCEL_REPORTE = "Reportes"
 
 # Lista de tablas a exportar:
 # Cada item = (schema, tabla)
 
-TABLAS_EXCEL = [
+TABLAS_EXCEL_REPORTE = [
     ("reporte", "rdc01_final"),
     ("reporte", "rdc01_caratula"),
     ("reporte", "rdc20_final_1"),
     ("reporte", "rdc20_final_2"),
     ("reporte", "rdc20_final_3"),
     ("reporte", "rdc20_final_4"),
-    ("reporte", "rdc20_caratula"),
+    ("reporte", "rdc20_caratula")
 ]
 
+# ==============================
+# CONFIGURACIÓN DETALLE REPORTE
+# ==============================
 
-# Instancia única del exportador
-exporter = MultiTableToExcelExporter(conn_params=PG_CONN, name=GROUP_NAME)
+GROUP_NAME_DETALLE = "exporta_detalle_excel"
+NOMBRE_EXCEL_DETALLE = "Reportes_detalle"
+
+# Lista de tablas a exportar:
+# Cada item = (schema, tabla)
+
+TABLAS_EXCEL_DETALLE = [
+    ("reporte", "rdc01_detalle"),
+    ("reporte", "rdc20_detalle_1"),
+    ("reporte", "rdc20_detalle_2"),
+    ("reporte", "rdc20_detalle_3"),
+    ("reporte", "rdc20_detalle_4")
+]
 
 
 # ==============================
@@ -36,22 +50,25 @@ exporter = MultiTableToExcelExporter(conn_params=PG_CONN, name=GROUP_NAME)
 # ==============================
 
 
-def build_group(dag, tablas=TABLAS_EXCEL, out_dir=EXCEL_DIR, nombre_excel=NOMBRE_EXCEL):
+def _build_excel_group(group_name, tablas, out_dir, nombre_excel):
     """
-    TaskGroup para exportar múltiples tablas a un archivo Excel.
+    TaskGroup para exportar múltiples tablas a un archivo Excel con multiples hojas.
 
     Parámetros:
-        dag           → instancia del DAG padre
+        group_name    → para poder generar grupos ID distintos y generar mas de un excel 
         tablas        → lista de tuplas (schema, tabla)
         out_dir       → directorio destino del archivo
         nombre_excel  → nombre base del archivo Excel
 
     """
+    
+    # Instancia única del exportador
+    exporter = MultiTableToExcelExporter(conn_params=PG_CONN, name=group_name)
 
-    with TaskGroup(group_id=GROUP_NAME, dag=dag) as group:
+    with TaskGroup(group_id=group_name) as group:
 
-        tarea = PythonOperator(
-            task_id="exporta_reportes_multitablas",
+        PythonOperator(
+            task_id="exporta_excel",
             python_callable=exporter.exportar,
             op_kwargs={
                 "tablas": tablas,
@@ -61,3 +78,36 @@ def build_group(dag, tablas=TABLAS_EXCEL, out_dir=EXCEL_DIR, nombre_excel=NOMBRE
         )
 
     return group
+
+
+# ==============================================
+# GENERACION DE EXCEL PARA REPORTES Y DETALLES
+# ==============================================
+
+
+def build_group(dag):
+    """
+    Builder estándar para el orquestador REDEC.
+    Retorna un único TaskGroup "contenedor".
+    """
+
+    with TaskGroup(group_id="exporta_excels", dag=dag) as main_group:
+
+        grupo_reporte = _build_excel_group(
+            GROUP_NAME_REPORTE,
+            TABLAS_EXCEL_REPORTE,
+            EXCEL_DIR,
+            "Reportes"
+        )
+
+        grupo_detalle = _build_excel_group(
+            GROUP_NAME_DETALLE,
+            TABLAS_EXCEL_DETALLE,
+            EXCEL_DIR,
+            "Reportes_detalle"
+        )
+
+        grupo_reporte >> grupo_detalle
+
+    return main_group
+
